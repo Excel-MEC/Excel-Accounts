8e6ccc3497b031f422e97474162f4197febc96bc
AjeshKumarS <ajeshkumar36020@gmail.com>
2020-02-03 14:33:59 +0530

    Initial Commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    // [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("values");
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(id);
+        }
+    }
+}
