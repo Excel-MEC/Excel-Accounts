c28bd5751501b49bfd5c9f0f624b0c31d405382f
AjeshKumarS <ajeshkumar36020@gmail.com>
2020-02-11 10:12:10 +0530

    Added Postgress database

---
+++ Models/Value.cs
@@ -0,0 +1,8 @@
+namespace Excel_Accounts_Backend.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,16 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Excel_Accounts_Backend.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace API
+namespace Excel_Accounts_Backend
 {
     public class Startup
     {
@@ -25,7 +27,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adding Controller as service
             services.AddControllers();
+            // Adding HttpClient service for fetching external Apis
+            services.AddHttpClient();
+            // Add Database to the Services
+            services.AddDbContext<DataContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,10 +43,16 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
+            // Uncomment following line when having https
             // app.UseHttpsRedirection();
 
+            // Allow Cross origin requests
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            // Use Routing
             app.UseRouting();
 
+            // User authorization
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
