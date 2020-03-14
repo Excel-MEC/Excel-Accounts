30f3d849b31e38332ebfbd444b556d342ec463e1
PriyangaPKini <priyangapkini99@gmail.com>
2020-03-14 16:41:50 +0530

    Adding a route for qrcode

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -62,10 +62,46 @@ namespace Excel_Accounts_Backend.Controllers
         }
 
         [HttpPost("qrcode")]
-        public async Task<User> CreateQrCode([FromForm]User user)
+
+        public async Task<string> CreateQrCode([FromForm]string ExcelId)
         {
-            User newUser = await _authRepository.Register(user);
-            return newUser;
+            DataForFileUploadDto qrCodeDto = new DataForFileUploadDto();
+            qrCodeDto.Name = ExcelId;
+            Bitmap qrCodeImage = GenerateQrCode(qrCodeDto);
+            BitmapToImageFile(qrCodeImage, qrCodeDto);
+
+            await UploadFile(qrCodeDto);
+            string ImageUrl = _configuration.GetValue<string>("CloudStorageUrl") + qrCodeDto.ImageStorageName;
+            return ImageUrl;
+        }
+
+        // Generates Bitmap image of the string
+        private static Bitmap GenerateQrCode(DataForFileUploadDto qrCodeDto)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrCodeDto.Name, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return qrCodeImage;
+        }
+
+        // Converts the Bitmap Image to a PNG File
+        private static void BitmapToImageFile(Bitmap qrCodeImage, DataForFileUploadDto qrCodeDto)
+        {
+            byte[] bitmapBytes = BitmapToBytes(qrCodeImage);
+            var stream = new MemoryStream(bitmapBytes);
+            IFormFile Image = new FormFile(stream, 0, bitmapBytes.Length, qrCodeDto.Name, qrCodeDto.Name + ".png");
+            qrCodeDto.Image = Image;
+        }
+
+        // Converts the Bitmap to byte array
+        private static byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
         }
     }
 }
